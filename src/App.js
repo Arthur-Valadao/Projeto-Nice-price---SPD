a72abac22cb106072eebc3c066cbf421cceb8109
Arthur Gomes Valadão <arthur.valadao@itriad.org.br>
2022-05-23 10:17:31 -0400

    homePage done

--- src/App.js
+++ src/App.js
@@ -12,19 +12,22 @@ import {
   Card
 } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import first from './first.svg';
-import second from './second.svg';
-import third from './third.svg'
-
+import logo from '../src/assets/logo.jpg';
+import first from '../src/assets/promocao.jpg';
+import second from '../src/assets/promocao-02.jpg';
+import third from '../src/assets/promocao-03.jpg';
+import hortifruti from '../src/assets/hortifruti.jpg';
+import frios from '../src/assets/frios.jpg';
+import padaria from '../src/assets/padaria.jpg';
 
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
         <Navbar bg="light" expand="lg">
           <Container fluid>
-            <Navbar.Brand href="#">Nice Price</Navbar.Brand>
+            <Navbar.Brand href="#"><img className="Logo" src={logo} alt="" /></Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
               <Nav
@@ -33,114 +36,64 @@ function App() {
                 navbarScroll
               >
                 <Nav.Link href="#action1">Home</Nav.Link>
-                <Nav.Link href="#action2">Link</Nav.Link>
-                <NavDropdown title="Link" id="navbarScrollingDropdown">
-                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Something else here
-                  </NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link href="#" disabled>
-                  Link
-                </Nav.Link>
+                <Nav.Link href="#action2">Supermercados</Nav.Link>
+                <Form className="d-flex">
+                  <FormControl
+                    type="search"
+                    placeholder="Busca"
+                    className="CampoBusca me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Pesquisar</Button>
+                </Form>
               </Nav>
-              <Form className="d-flex">
-                <FormControl
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
+
             </Navbar.Collapse>
           </Container>
         </Navbar>
       </header>
-      <body>
+      <body className="App">
         <Carousel className="Carousel" interval={2000}>
           <Carousel.Item interval={1000}>
             <img
-              className="d-block w-100"
+              className="d-block w-100 ImageCarousel"
               src={first}
               alt="First slide"
             />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item interval={1000}>
             <img
-              className="d-block w-100"
+              className="d-block w-100 ImageCarousel"
               src={second}
               alt="Second slide"
             />
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100"
+              className="d-block w-100 ImageCarousel"
               src={third}
               alt="Third slide"
             />
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
-        <FormControl
-          type="search"
-          placeholder="Search"
-          className="me-2 mt-5"
-          aria-label="Search"
-        />
         <CardGroup className="mt-5">
           <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
             <Card.Body>
-              <Card.Title>Card title</Card.Title>0
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.
-              </Card.Text>
+              <Card.Title>Hortifruti</Card.Title>
+              <Card.Img variant="top" src={hortifruti} className="ImageSize" />
             </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
           </Card>
           <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This card has supporting text below as a natural lead-in to additional
-                content.{' '}
-              </Card.Text>
+              <Card.Title>Frios</Card.Title>
+              <Card.Img variant="top" src={frios} className="ImageSize" />
             </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
           </Card>
           <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This card has even longer content than the first to
-                show that equal height action.
-              </Card.Text>
+              <Card.Title>Padaria</Card.Title>
+              <Card.Img variant="top" src={padaria} className="ImageSize" />
             </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
           </Card>
         </CardGroup>
       </body>
